d616135a28dff2325c91ba5983ac4e541e37e56e
sgrv13 <sofigrv13@gmail.com>
2025-10-29 17:19:07 -0600

    feat: Add user frontend module

--- php/src/db.php
+++ php/src/db.php
@@ -1,7 +1,7 @@
 <?php
 $uri = getenv('MONGO_URI') ?: 'mongodb://db:27017';
 try {
-    $manager = new MongoDB\Driver\Manager($uri); // Se ha completado la conexion
+    $manager = new MongoDB\Driver\Manager($uri); 
     
 } catch (Exception $e) {
     echo "<h2>Error de conexión ❌</h2>";
